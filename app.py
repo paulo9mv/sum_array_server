9fc9bbebe3451a2050cc0ca6fa8ce95ceb2c50a4
Paulo Viana <paulo9mv@yahoo.com.br>
2021-07-29 12:20:39 -0400

    refactor: remove unused file

--- app.py
+++ app.py
@@ -8,33 +8,25 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/equilibrium', methods=['POST'])
 @cross_origin()
-def hello():
+def postEquilibrium():
     content = request.json
-
 
     # Check if 'arr' prop exists
     if 'arr' not in content:
         return generateError("'arr' prop is missing"), 400
 
     arr = content['arr']
-    print('arrrr')
 
     # Check if 'arr' is a list
     if isinstance(arr, list):
         # Check if 'arr' contains only integers
         if all(isinstance(elem, int) for elem in arr):
             a = getIndexOfEquilibrium(arr)
-            print('hahaha')
-            print(a)
             
             return jsonify({"index":a})
         else:
-            response = generateError("only integer numbers are allowed")
-            
             return generateError("only integer numbers are allowed"), 400
     else:
-        response = generateError("array is expected")
-        
         return generateError("array is expected"), 400
 
 def generateError(msg):
